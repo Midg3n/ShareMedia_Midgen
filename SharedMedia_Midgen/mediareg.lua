817a16702833265a3923f2758459a40226a842f4
Midg3n <47797297+Midg3n@users.noreply.github.com>
2022-11-27 00:27:53 +0100

    Add files via upload

---
+++ SharedMedia_Midgen/mediareg.lua
@@ -0,0 +1,36 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "|cff0000ffUnivers|r", [[Interface\Addons\SharedMedia_Midgen\font\Univers.ttf]]) 
+LSM:Register("font", "|cff0000ffGothamNarrowUltra|r", [[Interface\Addons\SharedMedia_Midgen\font\GothamNarrowUltra.ttf]]) 
+
+-- -----
+--   SOUND
+-- ALL
+LSM:Register("sound", "|cff0000ffbell|r", [[Interface\Addons\SharedMedia_Midgen\sound\bell.ogg]])
+LSM:Register("sound", "|cff0000ffkick|r", [[Interface\Addons\SharedMedia_Midgen\sound\kick.ogg]])
+LSM:Register("sound", "|cff0000ffpotion please|r", [[Interface\Addons\SharedMedia_Midgen\sound\potionplease.ogg]])
+LSM:Register("sound", "|cff0000ffgavel|r", [[Interface\Addons\SharedMedia_Midgen\sound\gavel.ogg]])
+--DK
+LSM:Register("sound", "|cff0000ffamz please|r", [[Interface\Addons\SharedMedia_Midgen\sound\amzplease.ogg]])
+LSM:Register("sound", "|cff0000ffgrip|r", [[Interface\Addons\SharedMedia_Midgen\sound\grip.ogg]])
+--Warri
+LSM:Register("sound", "|cff0000ffshout please|r", [[Interface\Addons\SharedMedia_Midgen\sound\shoutplease.ogg]])
+--Shami
+LSM:Register("sound", "|cff0000ffwindrush|r", [[Interface\Addons\SharedMedia_Midgen\sound\windrushplease.ogg]])
+LSM:Register("sound", "|cff0000ffspirit link|r", [[Interface\Addons\SharedMedia_Midgen\sound\spiritlinkplease.ogg]])
+
+-- -----
+-- -----
+--   STATUSBAR
+-- -----
